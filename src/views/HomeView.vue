eab146d2762aae4b9cfd6a652bf02d4f9c2ccaff
HENG-VISOTHI <visothibd@gmail.com>
2023-05-25 00:24:02 +0700

    ADD-project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="home">
+
+    <div class="model">
+      <div class="row">
+        <div class="coupon-box1">
+          <h2 class="">SPRING SALE — 20% OFF WITH CODE: SPRING 
+          <RouterLink to="/product">SHOP NOW</RouterLink></h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="coupon-box">COUPON Saving up to 25%</div>
+        <div class="coupon-box">Free Delivery For spending up $100</div>
+        <div class="coupon-box">GOLD MEMBRSHIP up to 50%</div>
+      </div>
+    </div>
+    <!-- <br>
+    <div class="items" >
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+        <div class="item"><div class="box"><img src="../assets/images/item.jpg" alt=""></div></div>
+    </div>
+
+    <br>
+     -->
+     <div>
+    <!-- <h2>JSON Data:</h2> -->
+    <ul>
+      <li v-for="item in jsonData" :key="item.id">
+        <p>{{ item.name }}</p>
+        <p>{{ item.description }}</p>
+      </li>
+    </ul>
+  </div>
+      
+
+  </div>
+</template>
+<script>
+// export default {
+//   mounted() {
+//    const url = "https://fakestoreapi.com/products/1";
+//    async function getAllProducts(){
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+//    }
+//    getAllProducts();
+//   },
+// };
+export default {
+  // data() {
+  //   return {
+  //     jsonData: [
+  //       { id: 1, name: 'Item 1', description: 'Description for Item 1' },
+  //       { id: 2, name: 'Item 2', description: 'Description for Item 2' },
+  //       { id: 3, name: 'Item 3', description: 'Description for Item 3' }
+  //     ]
+  //   };
+  // }
+
+};
+
+</script>
+
+<style>
+
+h2 {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+@media (min-width: 2048px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+}
+
+
+
+.model{
+  width: 100%;
+  height: auto;
+  display: block;
+  /* flex-direction: row;
+  justify-content: space-around; */
+  margin-top: 50px;
+}
+.row{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  
+}
+.coupon-box1{
+  /* background-color: */
+  background-image: linear-gradient( 135deg, #FFF6B7 10%, #F6416C 100%);;
+  position: relative;
+  text-align: center;
+  flex: 1;
+  max-width: 100%;
+  height: 80px;
+  margin: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 5px rgba(181,225,235,255);
+}
+.coupon-box{
+  /* background-color: rgba(181,225,235,255); */
+  /* background-image: linear-gradient( 135deg, #FD6585 10%, #0D25B9 100%); */
+  background-image: linear-gradient( 135deg, #69FF97 10%, #00E4FF 100%);
+  position: relative;
+  flex: 1;
+  max-width: 400px;
+  height: 200px;
+  margin: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 5px rgba(181,225,235,255);
+  font-family: Rockwell;
+  font-size: 35px;
+  text-align: center;
+} @media (min-width: 2048px){
+  .coupon-box{
+    flex: 100%;
+  
+  }
+
+  
+}
+
+
+
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,278 @@
+<template>
+   <header>
+   
+    <nav class="nav">
+      <i class="uil uil-bars navOpenBtn"></i>
+      <a href="#" class="logo">HENG VISOTHI</a>
+      <ul class="nav-links">
+        <i class="uil uil-times navCloseBtn"></i>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+        <li><RouterLink to="/product">Product</RouterLink></li>
+        <!-- <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li> -->
+      </ul>
+      <i class="uil uil-search search-icon" id="searchIcon"></i>
+      <div class="search-box">
+        <i class="uil uil-search search-icon"></i>
+        <input type="text" placeholder="Search here..." />
+      </div>
+    </nav>
+
+   </header>
+  <RouterView />
+  <footer>
+    <div class="container">
+    <div class="footer-content">
+      <div class="footer-logo">
+        <a href="#">HENG VISOTHI</a>
+      </div>
+      <nav class="footer-nav">
+        <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+        <li><RouterLink to="/product">Product</RouterLink></li>
+        </ul>
+      </nav>
+    </div>
+    <p class="footer-text">&copy; 2023 Your Company. All rights reserved.</p>
+  </div>
+  </footer>
+</template>
+
+
+<script>
+export default {
+  mounted() {
+    const nav = document.querySelector(".nav");
+    const searchIcon = document.querySelector("#searchIcon");
+    const navOpenBtn = document.querySelector(".navOpenBtn");
+    const navCloseBtn = document.querySelector(".navCloseBtn");
+
+    searchIcon.addEventListener("click", () => {
+      nav.classList.toggle("openSearch");
+      nav.classList.remove("openNav");
+      if (nav.classList.contains("openSearch")) {
+        searchIcon.classList.replace("uil-search", "uil-times");
+      } else {
+        searchIcon.classList.replace("uil-times", "uil-search");
+      }
+    });
+
+    navOpenBtn.addEventListener("click", () => {
+      nav.classList.add("openNav");
+      nav.classList.remove("openSearch");
+      searchIcon.classList.replace("uil-times", "uil-search");
+    });
+
+    navCloseBtn.addEventListener("click", () => {
+      nav.classList.remove("openNav");
+    });
+  },
+};
+</script>
+<style>
+/* Google Fonts - Poppins */
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+.nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 15px 200px;
+  background: #4a98f7;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+}
+.nav,
+.nav .nav-links {
+  display: flex;
+  align-items: center;
+}
+.nav {
+  justify-content: space-between;
+}
+a {
+  color: #fff;
+  text-decoration: none;
+}
+.nav .logo {
+  font-size: 22px;
+  font-weight: 500;
+}
+.nav .nav-links {
+  column-gap: 20px;
+  list-style: none;
+}
+.nav .nav-links a {
+  transition: all 0.2s linear;
+}
+.nav.openSearch .nav-links a {
+  opacity: 0;
+  pointer-events: none;
+}
+.nav .search-icon {
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+}
+.nav .search-box {
+  position: absolute;
+  right: 250px;
+  height: 45px;
+  max-width: 555px;
+  width: 100%;
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.2s linear;
+}
+.nav.openSearch .search-box {
+  opacity: 1;
+  pointer-events: auto;
+}
+.search-box .search-icon {
+  position: absolute;
+  left: 15px;
+  top: 50%;
+  left: 15px;
+  color: #4a98f7;
+  transform: translateY(-50%);
+}
+.search-box input {
+  height: 100%;
+  width: 100%;
+  border: none;
+  outline: none;
+  border-radius: 6px;
+  background-color: #fff;
+  padding: 0 15px 0 45px;
+}
+
+.nav .navOpenBtn,
+.nav .navCloseBtn {
+  display: none;
+}
+
+/* responsive */
+@media screen and (max-width: 1160px) {
+  .nav {
+    padding: 15px 100px;
+  }
+  .nav .search-box {
+    right: 150px;
+  }
+}
+@media screen and (max-width: 950px) {
+  .nav {
+    padding: 15px 50px;
+  }
+  .nav .search-box {
+    right: 100px;
+    max-width: 400px;
+  }
+}
+@media screen and (max-width: 768px) {
+  .nav .navOpenBtn,
+  .nav .navCloseBtn {
+    display: block;
+  }
+  .nav {
+    padding: 15px 20px;
+  }
+  .nav .nav-links {
+    position: fixed;
+    top: 0;
+    left: -100%;
+    height: 100%;
+    max-width: 280px;
+    width: 100%;
+    padding-top: 100px;
+    row-gap: 30px;
+    flex-direction: column;
+    background-color: #11101d;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    transition: all 0.4s ease;
+    z-index: 100;
+  }
+  .nav.openNav .nav-links {
+    left: 0;
+  }
+  .nav .navOpenBtn {
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  .nav .navCloseBtn {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  .nav .search-box {
+    top: calc(100% + 10px);
+    max-width: calc(100% - 20px);
+    right: 50%;
+    transform: translateX(50%);
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  }
+}
+
+footer {
+  background-color: #4a98f7;
+  color: #fff;
+  padding: 20px 0;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.footer-logo a {
+  color: #fff;
+  font-size: 24px;
+  text-decoration: none;
+}
+
+.footer-nav ul {
+  list-style-type: none;
+  display: flex;
+}
+
+.footer-nav li:not(:last-child) {
+  margin-right: 20px;
+}
+
+.footer-nav a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.footer-text {
+  text-align: center;
+  margin-top: 20px;
+}
+
+
+</style>
+
+<!-- <RouterLink to="/">Home</RouterLink>
+<RouterLink to="/about">About</RouterLink>
+<RouterLink to="/product">Product</RouterLink>
+<button @click="$emit('checkout')">Go to checkout</button> -->
